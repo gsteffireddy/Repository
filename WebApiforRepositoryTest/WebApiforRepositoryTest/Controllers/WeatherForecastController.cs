f32bf0a3a8238add76780a9cbc27d2773d05f33e
gsteffireddy <steffi.gurram@expeed.com>
2022-05-11 10:17:58 -0400

    freeze push

--- WebApiforRepositoryTest/WebApiforRepositoryTest/Controllers/WeatherForecastController.cs
+++ WebApiforRepositoryTest/WebApiforRepositoryTest/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApiforRepositoryTest.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "ChillLLy", "Cooooool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freeeeeeezing", "Bracing", "ChillLLy", "Cooooool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
