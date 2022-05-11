1a53004fe610fd9d84d5e1b3ebc43a5ca4228986
gsteffireddy <104652032+gsteffireddy@users.noreply.github.com>
2022-05-11 08:30:48 -0400

    FIRST CHANGE

--- WebApiforRepositoryTest/WebApiforRepositoryTest/Controllers/WeatherForecastController.cs
+++ WebApiforRepositoryTest/WebApiforRepositoryTest/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApiforRepositoryTest.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "ChillLLy", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
